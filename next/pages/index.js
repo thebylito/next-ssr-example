be8b3e1ab6df27597be07512ccae908814a54995
Welington da Silva <thebylito@gmail.com>
2019-10-03 10:10:47 -0400

    Refatoração do Feed de Logs

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,97 +1,42 @@
 import React from 'react';
-import Link from 'next/link'
-import Head from '../components/head'
-import Nav from '../components/nav'
 import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import Divider from '@material-ui/core/Divider';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import Avatar from '@material-ui/core/Avatar';
-import Typography from '@material-ui/core/Typography';
+import Nav from '../components/nav';
+import Head from '../components/head';
 
-const options = {
-  year: 'numeric',
-  month: 'numeric',
-  day: 'numeric',
-  hour: 'numeric',
-  minute: 'numeric',
-  second: 'numeric',
-  hour12: false
-
-};
-const formatarData = (data) => new Intl.DateTimeFormat('pt-BR', options).format(new Date(data));
-
-let connection;
+import LogCard from '../components/LogCard';
 
 export default function HomePage() {
-  const [socket, setSocket] = React.useState(null)
+  const [socket, setSocket] = React.useState(null);
 
   const [localState, setLocalState] = React.useState({
-    feed: [{
-      userId: "welington.martins",
-      debug: "sadsaiudbsadsad",
-      time: new Date(),
-    }],
-  })
+    feed: [],
+  });
 
-  const handleMessageAdd = message => {
-    //socket.emit('message', 'aaaaaaaaaaaaaaaa')
-    setLocalState(oldState => ({
-      feed: [message, ...oldState.feed]
-    }))
+  const handleMessageAdd = (message) => {
+    // socket.emit('message', 'aaaaaaaaaaaaaaaa')
+    setLocalState((oldState) => ({
+      feed: [message, ...oldState.feed],
+    }));
   };
 
-  const renderBody = (debug) => {
-    try {
-      const data = JSON.parse(debug)
-      return JSON.stringify(data, null, 2);
-    } catch (error) {
-      return debug;
-    }
-  }
 
-  React.useEffect(()=>{
-
-    const connection = require('../lib/socket').default;
-    const init = new connection('appLogs').connect();
+  React.useEffect(() => {
+    const Connection = require('../lib/socket').default;
+    const init = new Connection('appLogs').connect();
     setSocket(init);
     const subscribe = init.subscribe('appLogs');
     subscribe.on('appLogs', handleMessageAdd);
-    return ()=> {
+    return () => {
       socket.close();
-    }
-  }, [])
-    return (
-      <>
-        <Head title="Home" />
-          <Nav />
+    };
+  }, []);
+  return (
+    <>
+      <Head title="Home" />
+      <Nav />
       <List>
-      {localState.feed.map(item => (
-      <ListItem alignItems="flex-start">
-        <ListItemAvatar>
-          <Avatar alt="Remy Sharp" src={`http://coredata.sanesul.ms.gov.br/api/funcionarios/${item.userId}/foto`} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={item.userId}
-          secondary={
-            <div>
-              {formatarData(item.time)}
-              <pre>
-              <Typography
-                component="div"
-                variant="body3"
-                color="textPrimary"
-              >
-                {renderBody(item.debug)}
-              </Typography>
-              </pre>
-            </div>
-          }
-        />
-      </ListItem>
-      ))}
+        {localState.feed.map((item) => <LogCard item={item} />)}
       </List>
-      </>
-    )
-  }
+    </>
+  );
+}
