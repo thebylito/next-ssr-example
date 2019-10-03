88222388ab051cb39179975bcb070728ef4dde20
Welington da Silva <thebylito@gmail.com>
2019-10-03 10:10:22 -0400

    Instalacao e configuracao EsLint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "extends": "airbnb",
+    "rules": {
+      "arrow-parens": 0,
+      "react/prop-types": 0,
+      "global-require": 0,
+      "react/jsx-filename-extension": [1, {
+        "extensions": [".js", ".jsx"]
+      }],
+      "class-methods-use-this": 0,
+    },
+    globals: {
+      use: true
+    }
+};
