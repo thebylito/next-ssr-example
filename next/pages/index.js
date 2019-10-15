672ab184e2e93d7e714f13c23fc4a40f471aab4b
Welington da Silva <thebylito@gmail.com>
2019-10-15 13:58:34 -0400

    [WIP] Integração com redux

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import List from '@material-ui/core/List';
+import { useDispatch } from 'react-redux';
 import Nav from '../components/nav';
 import Head from '../components/head';
 import LogCard from '../components/LogCard';
 
-export default function HomePage() {
+
+import { withRedux } from '../lib/redux';
+// import useInterval from '../lib/useInterval';
+
+
+function HomePage() {
+  const dispatch = useDispatch();
+
+
   const [socket, setSocket] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(true);
 
@@ -43,3 +52,18 @@ export default function HomePage() {
     </>
   );
 }
+
+HomePage.getInitialProps = ({ reduxStore }) => {
+  // Tick the time once, so we'll have a
+  // valid time before first render
+  const { dispatch } = reduxStore;
+  // dispatch({
+  //   type: 'TICK',
+  //   light: typeof window === 'object',
+  //   lastUpdate: Date.now(),
+  // });
+
+  return {};
+};
+
+export default withRedux(HomePage);
