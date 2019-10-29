2e80b7cc08e1dbdbf605db5fa331d7c3588107f9
Welington da Silva <thebylito@gmail.com>
2019-10-29 13:45:31 -0400

    Componente para Exibição de titulos das páginas

---
+++ next/components/pages/shared/PaginaTitulo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Typography } from '@material-ui/core';
+
+function PaginaTitulo({ children, titulo }) {
+  return (
+    <Typography
+      color="primary"
+      style={{ textTransform: 'uppercase' }}
+      variant="h4"
+      align="center"
+    >
+      {children || titulo}
+    </Typography>
+  );
+}
+
+export default PaginaTitulo;
