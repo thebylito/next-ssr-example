42673ddd11a223fafd4875be4138dfb2146168a0
Welington da Silva <thebylito@gmail.com>
2019-10-29 10:18:37 -0400

    Exibição de mensagens enviadas para o usuario

--- next/pages/mensagens.js
+++ next/pages/mensagens.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
-  Grid, Hidden, Typography, Card, CardContent, List,
+  Grid, Hidden, Typography, Card, CardContent, List, Box, LinearProgress,
 } from '@material-ui/core';
 import withRedux from 'lib/redux';
 import Head from 'components/head';
@@ -9,11 +9,14 @@ import Nav from 'components/nav';
 import UserCard from 'components/pages/perfil/UserCard';
 import { makeStyles } from '@material-ui/styles';
 import { Creators } from 'appStore/ducks/mensagem/lista';
+import UserAvatar from 'components/pages/dashboard/UserAvatar';
+import moment from 'moment';
+import Scrollbar from 'react-scrollbars-custom';
 
 const useStyles = makeStyles({
   card: {
-    maxWidth: 400,
     marginBottom: 10,
+    margin: '0 10px',
   },
   media: {
     height: 140,
@@ -23,7 +26,7 @@ const useStyles = makeStyles({
   },
 });
 
-function Pagamentos() {
+function Mensagens() {
   const classes = useStyles();
   const dispatch = useDispatch();
   const { mensagens, mensagensLoading } = useSelector(state => state.mensagem.lista);
@@ -36,39 +39,54 @@ function Pagamentos() {
     <>
       <Head title="Minhas mensagens" />
       <Nav />
-      <Grid
-        container
-        direction="row"
-      >
+      {mensagensLoading && <LinearProgress color="primary" />}
+      <Grid container direction="row">
         <Hidden xsDown>
-          <Grid sm={5} md={4} direction="column" item>
+          <Grid sm={5} md={4} item>
             <UserCard />
           </Grid>
         </Hidden>
-        <Grid direction="column" xs={12} sm={7} md={8} item>
+        <Grid xs={12} sm={7} md={8} item>
           <Typography variant="h5" style={{ textTransform: 'uppercase' }} align="center">
             Minhas Mensagens
           </Typography>
-          <Grid sm={12} md={12} spacing={5} direction="column">
+          <Scrollbar style={{ width: '100%', height: '86vh' }}>
             <List>
-              {mensagens.map(mensagem => (
-                <Card className={classes.card}>
-                  <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2" className={classes.cardTitleText}>
-                      {mensagem.titulo}
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                      {mensagem.mensagem}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              ))}
+              {mensagens.map(mensagem => {
+                const dataHoraEnvio = moment(mensagem.dataHoraEnvio);
+                return (
+                  <Card key={mensagem.id} className={classes.card}>
+                    <CardContent>
+                      <Box display="flex" flexDirection="row" alignItems="center">
+                        <UserAvatar size={50} matricula={mensagem.usuarioOrigem.login} />
+                        <Box style={{ marginLeft: 10 }} display="flex" flexDirection="column">
+                          <Typography variant="body1" style={{ textTransform: 'uppercase' }}>
+                            {mensagem.usuarioOrigem.nome}
+                          </Typography>
+                          <Typography color="textSecondary" variant="caption" style={{ textTransform: 'uppercase' }}>
+                            {mensagem.usuarioOrigem.cargo_descricao}
+                          </Typography>
+                          <Typography color="textSecondary" variant="caption" style={{ textTransform: 'uppercase' }}>
+                            {mensagem.usuarioOrigem.gerencia_nome}
+                          </Typography>
+                          <Typography color="textSecondary" variant="caption" style={{ textTransform: 'uppercase' }}>
+                            {`${dataHoraEnvio.format('DD/MM/YYYY HH:mm')} - ${dataHoraEnvio.fromNow()}`}
+                          </Typography>
+                        </Box>
+                      </Box>
+                      <Typography variant="body2" component="p" style={{ marginTop: 10 }}>
+                        {mensagem.mensagem}
+                      </Typography>
+                    </CardContent>
+                  </Card>
+                );
+              })}
             </List>
-          </Grid>
+          </Scrollbar>
         </Grid>
       </Grid>
     </>
   );
 }
 
-export default withRedux(Pagamentos);
+export default withRedux(Mensagens);
