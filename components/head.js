6645f12312a785ae321b421a592ecf7d993c9b62
Welington da Silva <thebylito@gmail.com>
2019-11-13 15:43:38 -0400

    Adicionado Favicon Sanesul

--- components/head.js
+++ components/head.js
@@ -14,8 +14,7 @@ const Head = (props) => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="icon" sizes="192x192" href="/static/touch-icon.png" />
     <link rel="apple-touch-icon" href="/static/touch-icon.png" />
-    <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
-    <link rel="icon" href="/static/favicon.ico" />
+    <link rel="icon" href="/static/favicon-32x32.png" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
     <meta property="og:url" content={props.url || defaultOGURL} />
