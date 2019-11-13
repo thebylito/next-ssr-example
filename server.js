488543b48e6ee8f6a45544711364351a70df6b1e
Welington da Silva <thebylito@gmail.com>
2019-11-13 15:45:02 -0400

    Adicionado arquivo para iniciar o servidor
    - Adicionado arquivo deploy.tar ao gitignore

---
+++ server.js
@@ -0,0 +1,21 @@
+// This file doesn't go through babel or webpack transformation.
+// Make sure the syntax and sources this file requires are compatible with the current node version you are running
+// See https://github.com/zeit/next.js/issues/1245 for discussions on Universal Webpack or universal Babel
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000;
+const app = next({ dev })
+const handle = app.getRequestHandler()
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, err => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
